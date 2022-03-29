fe93797c6f51e8460d85753be3ae7fa22d458156
Zumry <amzumry@gmail.com>
2022-03-29 16:39:55 +0530

    created question controller

---
+++ controllers/questionController.js
@@ -0,0 +1,86 @@
+const Question = require("../models/Question");
+
+exports.addQuestion = async (req,res,next) =>{
+    console.log(req.body);
+    await Question.insertMany(req.body).then((question)=> {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(question);
+    },(err) => {
+        next(err);
+    })
+    .catch((err) =>{
+        next(err);
+    })
+};
+
+exports.getQuestionById = async (req,res,next) => {
+    await Question.findById(req.params.id)
+    .then((question) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(question);
+    },(err) => {
+        next(err);
+    })
+    .catch((err) => {
+        next(err);
+    })
+};
+
+exports.getQuestionByTopicID = async (req,res,next) => {
+    await Question.find({topicId:req.params.id})
+        .then((question) => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(question);
+        },(err) => {
+            next(err);
+        })
+        .catch((err) => {
+            next(err);
+        })
+};
+
+exports.getAllQuestion = async (req,res,next) =>{
+    await Question.find({})
+        .then((question) =>{
+            res.statusCode = 200;
+            res.setHeader("Content-Type","application/json")
+            res.json(question);
+        },(err) =>{
+            next(err);
+        })
+        .catch((err) =>{
+            next(err);
+        })
+
+};
+
+exports.deleteQuestion = async (req, res, next) => {
+    await Question.deleteMany({})
+        .then(() => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json({data:"Success"});
+        },(err) => {
+            next(err);
+        })
+        .catch((err) => {
+            next(err);
+        })
+};
+
+// const updateQuestion = async (id,question) => {
+//     return await Question.findByIdAndUpdate(id,{
+//         $set:{question:question.question,answer:question.answer,correctAnswer:question.correctAnswer}
+//     },{ new :true })
+//         .then((question) => {
+//             return question;
+//         },(err) => {
+//             next(err);
+//         })
+//         .catch((err) => {
+//             next(err);
+//         })
+// };
