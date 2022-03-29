e4354129a391f73037a616b8c214fd40cecb5f09
Zumry <amzumry@gmail.com>
2022-03-29 15:46:04 +0530

    question model added

---
+++ model/question.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Question = new Schema(
+    {
+        topicId: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "Topic",
+        },
+        question: {
+            type: String,
+            default: "",
+            required: true,
+        },
+        answer: {
+          type: String,
+          default: "",
+          required: true,
+        },
+        correctAnswer:{
+            type: String,
+            default: "",
+            required: true,
+        }
+      },
+      {
+        timestamps: true,
+      }
+);
+
+module.exports = mongoose.model("Question", Question);
